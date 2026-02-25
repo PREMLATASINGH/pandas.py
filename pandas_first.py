3ee28d08c2ec67a4fae70f5eefba22fd9536fe4c
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-24 23:20:32 -0500

    first commit

---
+++ pandas_first.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# Create a sample DataFrame
+data = {    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [25, 30, 35, 40, 45],    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']}
+df = pd.DataFrame(data)     
+# Display the first few rows of the DataFrame
+print(df.head())
+data1=[1,2,3,4,5]
+s=pd.Series(data1)
+print(s)
