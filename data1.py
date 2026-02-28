9001e1d3b38267b29296d0b3599a3ad4f2152779
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-02-27 22:00:28 -0500

    added

--- data1.py
+++ data1.py
@@ -2,7 +2,7 @@ import pandas as pd
 data={
     "Name":["A","B","C","D"],
     "Age":[23,21,25,28],
-    "Salary":[40000,50000,45000,60000]
+    "Salary":[30000,50000,45000,60000]
 }
 df=pd.DataFrame(data)
 print(df)
@@ -10,5 +10,7 @@ df["Bonus"]=df["Salary"]*0.10
 print(df)
 df["Salary"]=df["Salary"]+5000
 print(df)
-df[df["Salary"]>45000]
-print(df)
+df1=df[df["Salary"]>45000]
+print(df1)
+#df[(df["Salary"]>45000)&(df["Age"]>24)]
+#print(df)
