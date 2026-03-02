8cd80574936d0b7055e6bf845bcd0fd3cc59d1b8
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-02 15:18:20 -0500

    added

--- read_data.py
+++ read_data.py
@@ -1,7 +1,5 @@
 import pandas as pd
 from io import StringIO
-Data={
-    "employee_name":"prema","email":"singh@gmail.com","job_profile":[{"title1":"team_lead","title2":"data_analyst"}]
-}
+Data='{"employee_name":"prema","email":"singh@gmail.com","job_profile":[{"title1":"team_lead","title2":"data_analyst"}]}'
 df=pd.read_json(StringIO(Data))
 print(df)
