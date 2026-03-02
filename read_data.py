19abffdd296b72a1525e6d336ad32f046063096b
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-02 15:19:23 -0500

    added

--- read_data.py
+++ read_data.py
@@ -3,3 +3,4 @@ from io import StringIO
 Data='{"employee_name":"prema","email":"singh@gmail.com","job_profile":[{"title1":"team_lead","title2":"data_analyst"}]}'
 df=pd.read_json(StringIO(Data))
 print(df)
+print(type(df))
