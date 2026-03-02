b89289a095779f59a50f2cf9d8ea85c23033c22e
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-02 15:11:50 -0500

    added

---
+++ read_data.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from io import StringIO
+data={
+    'employee_name':'prema','email':'singh@gmail.com','job_profile':[{'title1':'team_lead','title2':'data_analyst'}]
+}
+df=pd.read_json(StringIO(data))
+print(df)
