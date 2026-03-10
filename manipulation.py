c3254674311d613ce6df009481d326e822ea1e58
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-10 16:04:11 -0400

    added

--- manipulation.py
+++ manipulation.py
@@ -47,6 +47,8 @@ df3=pd.merge(df1,df2,on="Key",how="right")
 print(df3)
 df3=pd.merge(df1,df2,on="Key",how="outer")
 print(df3)
+df3=pd.merge(df1,df2,on="Key",how="left")
+print(df3)
 x=df3['Value1'].fillna(0)
 print(x)
 df3['Value1']=x
