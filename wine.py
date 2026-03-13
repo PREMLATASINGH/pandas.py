0d80c270e1d24dce306686e2adb5526a32f344b4
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-12 23:42:22 -0400

    pairplot

--- wine.py
+++ wine.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 df = pd.read_csv('wine_data.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
 pd.plotting.scatter_matrix(df, figsize=(10, 10))
+plt.show()
+sns.pairplot(df)
 plt.show()
