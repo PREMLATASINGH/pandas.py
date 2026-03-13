901972e718c0cb63783634ceb5a5bc4b08758629
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-12 23:40:04 -0400

    added

--- wine.py
+++ wine.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('wine_data.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
+# Visualize the distribution of wine quality
+plt.figure(figsize=(10, 6))
+plt.hist(df['quality'], bins=10, edgecolor='black')
+plt.title('Distribution of Wine Quality')
+plt.xlabel('Quality')
+plt.ylabel('Frequency')
+plt.show()
