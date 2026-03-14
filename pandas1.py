3a2016b276e5aeafc665dfced3a147ba4c622a51
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-14 14:13:42 -0400

    scatter

--- pandas1.py
+++ pandas1.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('wine_data.csv')
 print(df.head())
 print(df.info())
 print(df.describe())
 print(df.corr())
 print(df.isnull().sum())
-print(df['quality'].value_counts())
+pd.plotting.scatter_matrix(df, figsize=(10, 10))
+plt.show()
+
