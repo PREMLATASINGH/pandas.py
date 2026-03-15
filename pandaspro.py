6c261126ca961e0f38a7f732972210589f6bec0b
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-15 14:58:06 -0400

    added new file

---
+++ pandaspro.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+data={
+    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [25, 30, 35, 40, 45],
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+}
+df=pd.DataFrame(data)
