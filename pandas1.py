0aca4a944a4313494f7b4655a1a5e49b20858e7d
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-14 20:50:01 -0400

    added heatmap

--- pandas1.py
+++ pandas1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 df = pd.read_csv('wine_data.csv')
 print(df.head())
 print(df.info())
@@ -8,4 +9,7 @@ print(df.corr())
 print(df.isnull().sum())
 pd.plotting.scatter_matrix(df, figsize=(10, 10))
 plt.show()
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.show()
+
 
