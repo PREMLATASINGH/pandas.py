cc1c294f36f66447be2d5168b549a285663c8020
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-16 14:39:13 -0400

    added new file

---
+++ pro2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df={'product':['freeze','vegetable','dairy','meat','grain','bakery','fruit','snacks','condiments','beverages','other'],
+    'sales':[100,150,200,250,300,350,400,450,500,550,600],
+    'profit':[20,30,40,50,60,70,80,90,100,110,120],
+    'category':['food','food','food','food','food','food','food','food','food','beverage','other']}
+df=pd.DataFrame(df)
+print(df)
