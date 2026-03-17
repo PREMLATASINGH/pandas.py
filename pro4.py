5a58921c6b1c8cc1ade021d47a20a0bcd2e532f1
PREMLATASINGH <56335900+PREMLATASINGH@users.noreply.github.com>
2026-03-17 14:15:33 -0400

    added head

--- pro4.py
+++ pro4.py
@@ -9,3 +9,4 @@ df={'product_id':['p001','p002','p003','p004','p005','p006','p007','p008','p009'
     'region':['North America','Europe','Asia','North America','Europe','Asia','North America','Europe','Asia','North America']}
 df=pd.DataFrame(df)
 print(df)
+print(df.head())
